fe5f54d1c97179102d394c7cf92f5a7be0c1f051
Pablo-Es <szyposz@gmail.com>
2018-02-06 22:05:18 +0100

    create app, add players

---
+++ zadanie-11/src/data/players.js
@@ -0,0 +1,13 @@
+export default [
+    {id: 1, name: 'Anatolyj', level: 40, bio: 'age 50, with one hand player'},
+    {id: 2, name: 'Maksym', level: 9, bio: 'age 34, clubs: Arka Noego'},
+    {id: 3, name: 'Natasha', level: 15, bio: 'age 22, club: Sankt Petersunrg'},
+    {id: 4, name: 'Maggie', level: 49, bio: 'age 57'},
+    {id: 5, name: 'Paolo', level: 24, bio: 'age 49'},
+    {id: 6, name: 'Joseph', level: 7, bio: 'age 78'},
+    {id: 7, name: 'Stiepan', level: 50, bio: 'age 38'},
+    {id: 8, name: 'Pietia', level: 30, bio: 'age 18'},
+    {id: 9, name: 'Oleg', level: 60, bio: 'age 27'},
+    {id: 10, name: 'Gawron', level: 47, bio: 'age 28'},
+    
+];
