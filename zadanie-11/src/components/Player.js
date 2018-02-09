dfa78bfd5f7232cf635abc764b7eb5b594efc6e3
Pablo-Es <szyposz@gmail.com>
2018-02-09 17:14:36 +0100

    ready to review @pukie

--- zadanie-11/src/components/Player.js
+++ zadanie-11/src/components/Player.js
@@ -5,11 +5,14 @@ import { Link } from 'react-router-dom';
 export default (props) => {
     const playerToDisplay = props.match.params.id;
     const player = players[playerToDisplay];
-    return <React.Fragment>
+    return player?
+        <React.Fragment>
         <h2>Gracz</h2>
             <p>Nazwa: {player.name}</p>
         <p>Poziom: {player.level}</p>
         <p>Życiorys: {player.bio}</p>
         <Link to='/'>Powrót na stronę główną</Link>
     </React.Fragment>
+        :
+        <p>Brak zawodnika</p>
 }
