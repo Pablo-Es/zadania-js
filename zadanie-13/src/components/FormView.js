9ed5854b031543c5884dc6f6e60012df93af4fc0
Pablo-Es <szyposz@gmail.com>
2018-02-21 22:10:56 +0100

    added header to app

--- zadanie-13/src/components/FormView.js
+++ zadanie-13/src/components/FormView.js
@@ -1,28 +1,16 @@
 import React, { Component } from 'react'
-import { Button, Checkbox, Form, Grid } from 'semantic-ui-react'
+import { Header } from 'semantic-ui-react'
+import AddProfile from './AddProfile'
 
 class FormView extends Component {
   render() {
     return (
       <React.Fragment>
-        <Grid container>
-          <Grid.Column>
-            <Form>
-              <Form.Field>
-                <label>First Name</label>
-                <input placeholder="First Name" />
-              </Form.Field>
-              <Form.Field>
-                <label>Last Name</label>
-                <input placeholder="Last Name" />
-              </Form.Field>
-              <Form.Field>
-                <Checkbox label="I agree to the Terms and Conditions" />
-              </Form.Field>
-              <Button type="submit">Submit</Button>
-            </Form>
-          </Grid.Column>
-        </Grid>
+        <Header as="h1" textAlign="center">
+          Zarządzanie listą kontaktów
+        </Header>
+
+        <AddProfile />
       </React.Fragment>
     )
   }
